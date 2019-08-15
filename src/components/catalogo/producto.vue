717228f83cda56c9566d736f7d36e714e4979658
Fredy <freddyarh13@gmail.com>
2019-08-15 17:38:21 -0500

    Catalogo-Images

--- src/components/catalogo/producto.vue
+++ src/components/catalogo/producto.vue
@@ -6,57 +6,60 @@
                 
                 <div class="row line-background bg-primary"></div>
                 <div class="row">
-                    <div class="col-auto col-md-4 col-lg-auto">
-                        <div class="bg-white card d-md-block d-none h-100 position-relative shadow">
+                    <div class="col-auto col-md-4 col-lg-auto d-md-block d-none">
+                        <div class="bg-white card h-100 position-relative shadow">
                             <div class="fondo_catalogo position-absolute bg-info"></div>
                             <div class="align-items-center bg-white d-flex h-100 position-relative">
                                 <img :src="refrigerados" class="refri card-img-top img-fluid">
+                                <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                             </div>   
                         </div>                      
                     </div>
                     <div class="col">
                         <div class="row d-flex">
-                            <div class="col">
-                                <div class="card mb-3 mt-5 mt-md-0 position-relative shadow">
+                             <div class="col-sm-6 d-md-none">
+                                <div class="mb-3 mt-md-0 respon_mas position-relative">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative pb-3 bg-white">
-                                    	<img :src="refrigerados" class="img-fluid w-50">
+                                    <div class="position-relative pb-3 bg-white catalogo_border shadow">
+                                        <img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>  
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                               <div class="card shadow mb-3 position-relative">
+                            <div class="col-sm-6">
+                                <div class="mb-3 mt-md-0 position-relative">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative pb-3  bg-white">
-                                    	<img :src="refrigerados" class="img-fluid w-50">
+                                    <div class="position-relative pb-3 bg-white catalogo_border shadow">
+                                        <img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                               <div class="position-relative">
+                                    <div class="fondo_catalogo position-absolute bg-info"></div>
+                                    <div class="bg-white catalogo_border mb-3 mb-sm-0 pb-3 position-relative shadow">
+                                        <img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>   
                                     </div>
                                 </div>
                             </div>
         
-                            <div class="w-100"></div>
-        
-                            <div class="col">
-                                <div class="card shadow mb-3 position-relative">
+                            <div class="col-sm-6">
+                                <div class="mb-3 position-relative respon_menos">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
+                                    <div class="position-relative  pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="card shadow mb-3 mt-n5 mt-md-0 position-relative">
+                            <div class="col-sm-6 offset-md-0 offset-sm-6">
+                                <div class="mb-3 mt-md-0 position-relative res_mas">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
+                                    <div class="position-relative  pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="w-100"></div>
-                            <div class="col-6">
-                                <div class="shadow mb-3 position-relative d-md-none">
-                                    <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
-                                    	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
@@ -64,67 +67,72 @@
                     </div>
                 </div>  
             </div>
-            </section>
+        </section>
             
-            <!-- TODO:Ejemplo Item -->
+            <!-- TODO:Ejemplo Item dos -->
             
-     <section class="mt-4 position-relative">   
-            <div class="container d-flex justify-content-center">  
+        <section class="position-relative"> 
+            <!-- TODO:Ejemplo Item dos-->
+            <div class="container d-flex justify-content-center">
+                
                 <div class="row line-background line-background2"></div>
-                <div class="row">
+                <div class="row">                   
                     <div class="col">
-                        <div class="row d-flex">
-                            <div class="col">
-                                <div class="card mb-3 mt-5 mt-md-0 position-relative shadow">
+                        <div class="row d-flex mt-md-4">
+                            <div class="col-sm-6">
+                                <div class="mb-3 position-relative res_menos" >
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative pb-3 bg-white">
+                                    <div class="position-relative pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                               <div class="card shadow mb-3 position-relative">
+                            <div class="col-sm-6">
+                               <div class="mb-3 position-relative">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative pb-3  bg-white">
+                                    <div class="position-relative pb-3  bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
         
-                            <div class="w-100"></div>
-        
-                            <div class="col">
-                                <div class="card shadow mb-3 position-relative">
+                            <div class="col-sm-6">
+                                <div class="mb-3 position-relative res_menos" >
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
+                                    <div class="position-relative  pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="card shadow mb-3 mt-n5 mt-md-0 position-relative">
+                            <div class="col-sm-6">
+                                <div class="mb-3 position-relative">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
+                                    <div class="position-relative  pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="w-100"></div>
-                            <div class="col-6">
-                                <div class="shadow mb-3 position-relative d-md-none">
+                            <div class="col-sm-6 d-md-none">
+                                <div class="mb-3 mt-md-0 position-relative res_menos">
                                     <div class="fondo_catalogo position-absolute bg-info"></div>
-                                    <div class="position-relative  pb-3 bg-white">
+                                    <div class="position-relative pb-3 bg-white catalogo_border shadow">
                                     	<img :src="refrigerados" class="img-fluid w-50">
+                                        <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-auto col-md-4 col-lg-auto">
-                        <div class="bg-white card d-md-block d-none h-100 position-relative shadow">
+                    <div class="col-auto col-md-4 col-lg-auto d-md-block d-none">
+                        <div class="bg-white h-100 position-relative shadow">
                             <div class="fondo_catalogo position-absolute bg-info"></div>
                             <div class="align-items-center bg-white d-flex h-100 position-relative">
-                                <img :src="refrigerados" class="refri card-img-top img-fluid">
+                                <img :src="snacks" class="refri card-img-top img-fluid">
+                                <span class="letra_catalogo position-absolute font-italic font-weight-bold">Refigerados</br> & canaelados</span>
                             </div>   
                         </div>                      
                     </div>
@@ -137,6 +145,15 @@
 <script>
     import pasta from './../../assets/images/product/pasta.png'
     import refrigerados from './../../assets/images/catalogo/refrigerados.png'
+    import aceites from './../../assets/images/catalogo/aceites.png'
+    import chocolates from './../../assets/images/catalogo/chocolates.png'
+    import granolas from './../../assets/images/catalogo/granolas.png'
+    import mercado from './../../assets/images/catalogo/mercado.png'
+    import ninos from './../../assets/images/catalogo/ninos.png'
+    import panaderia from './../../assets/images/catalogo/panaderia.png'
+    import snacks from './../../assets/images/catalogo/snacks.png'
+    import suplementos from './../../assets/images/catalogo/suplementos.png'
+    import vinagres from './../../assets/images/catalogo/vinagres.png'
     import items from './items.vue'
     import item from './item.vue'
 
@@ -145,7 +162,17 @@
         data() {
             return {
                 pasta:pasta,
-                refrigerados:refrigerados
+                refrigerados:refrigerados,
+                aceites:aceites,
+                chocolates:chocolates,
+                granolas:granolas,
+                mercado:mercado,
+                ninos:ninos,
+                panaderia:panaderia,
+                snacks:snacks,
+                suplementos:suplementos,
+                vinagres:vinagres
+
             }
         },
         components: {
@@ -155,6 +182,21 @@
 </script>
 
 <style scoped>
+/* .back{
+    background-image: url(/img/refrigerados.fa2de2ba.png);
+    width: 21rem;
+    background-size: contain;
+    background-repeat: no-repeat;
+} */
+.letra_catalogo{
+    top: 47%;
+    left: 49%;
+    color:#00D0D2;
+    font-size:20px;
+}
+.catalogo_border{
+    border-radius: 12px;
+}
 .mercado_letra{
     font-family: "Varela Round" Bold Italic;
     font-size: 22px;
@@ -177,21 +219,45 @@
     position: absolute;
     width: 100%;
     height: 33px;
-    top: 71%;
+    top: 54%;
 }
 .line-background2{
     background-color: rgba(242, 240, 242, 1);
 
 }
-
+@media(min-width: 576px) and (max-width: 767px) {
+    .respon_mas{
+        margin-top: 50px;
+    }
+    .respon_menos{
+        margin-top: -50px;
+    }
+    .res_mas{
+        margin-top: -30px;
+    }
+    .res_menos{
+        margin-top: -137px;
+    }
+    .letra_catalogo{
+        top: 35%;
+        font-size:15px;
+    }
+}
 @media(min-width: 768px) {
         .mercado_letra{
         font-size: 32px;
+    }
+        .letra_catalogo{
+        top: 39%;
+        font-size:16px;
     }
 }
 @media(min-width: 992px) {
         .refri{
         width: 75%;
+    }
+    .letra_catalogo{
+        font-size:20px;
     }
 }
 
