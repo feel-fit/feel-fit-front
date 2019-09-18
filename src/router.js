866cdb021606ca3136b659dd9e95cc7539572411
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-09-18 17:45:41 -0500

    pagina registro
    * manutención pagina registro

--- src/router.js
+++ src/router.js
@@ -105,6 +105,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Deseos.vue')
-    }
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Registro.vue')
+    },
   ]
 })
