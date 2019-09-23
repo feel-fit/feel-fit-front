d86c78efb85e13195d7c6c59ac34d93ea90018d5
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-09-23 10:45:33 -0500

    modal descuento

--- src/components/pagos/modalDescuento.vue
+++ src/components/pagos/modalDescuento.vue
@@ -9,21 +9,21 @@
       aria-labelledby="modalDescuentoTitle"
       aria-hidden="true"
     >
-      <div class="modal-dialog modal-dialog-scrollable" role="document">
+      <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content btn-lg">
           <div class="modal-header border-0">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
+          <div class="modal-body text-center">
               <img src="./../../assets/images/pagos/descuento.png" class="img-fluid">
           </div>
           <div class="modal-footer d-flex flex-column border-0">
               <span class="text-muted font-italic small">Ha sido activado</span>
               <h3 class="text-primary font-weight-bold font-italic">Tu Descuento</h3>
               <p class="text-muted small">
-                Has sido seleccionado pra disfrutar de nuestro descuento
+                Has sido seleccionado para disfrutar de nuestro descuento
               </p>
           </div>
         </div>
--- src/views/GraciasPago.vue
+++ src/views/GraciasPago.vue
@@ -3,21 +3,26 @@
     <volver></volver>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-12 col-md-6">
-          <img src="./../assets/images/gracias/gracias.png" class="img-fluid">
+        <div class="col-md-6">
+          <img src="./../assets/images/gracias/gracias.svg" class="img-fluid" />
         </div>
-        <div class="col-12 col-md-6 pr-md-5">
-          <h3 class="text-center titulo text-primary font-italic font-weight-bold my-5 py-5">¡Gracias por tu compra!</h3>
-          <p class="text-center text">
-            Por favor revisa tu correo para ver la factura.<br> Si tienes alguna duda
-          </p>
-          <p class="text-center text">¡Cont&aacute;ctenos!</p>
-          <p class="text-right text-primary text2 mt-3 ">Cuidarte, se siente bien</p>
-          <div class=" text-right mt-4">
-            <router-link to="/" class="text-muted text-decoration-none">
-              Continua en el inicio
-              <font-awesome-icon icon="arrow-right" size="xs"/>
+        <div class="col-md-6">
+          <div class="container contenido w-50">
+            <h3
+              class="text-center titulo text-primary font-italic font-weight-bold my-5 py-5"
+            >¡Gracias por tu compra!</h3>
+            <p class="text-center text">
+              Por favor revisa tu correo para ver la factura.
+              <br />Si tienes alguna duda
+            </p>
+            <p class="text-center text">¡Cont&aacute;ctenos!</p>
+            <p class="text-right text-primary text2 mt-5">Cuidarte, se siente bien</p>
+            <div class="text-right mt-5">
+              <router-link to="/" class="text-muted text-decoration-none">
+                Continua en el inicio
+                <font-awesome-icon icon="arrow-right" size="xs" />
               </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -38,17 +43,20 @@ export default {
   border: 2px red solid;
 }
 
-.titulo{
+.titulo {
   font-size: 39px;
 }
 
-.texto{
+.texto {
   font-size: 20px;
-  color:#A3A3A3;
+  color: #a3a3a3;
 }
 
-.texto2{
+.texto2 {
   font-size: 17px;
 }
 
+.contenido {
+  min-width: 35rem;
+}
 </style>
