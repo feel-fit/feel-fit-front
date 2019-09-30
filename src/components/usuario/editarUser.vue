1cfc543857cfd245a20e4efc67899653e20d6254
mauricio suarez <mauroziux@gmail.com>
2019-09-30 14:38:14 -0500

    fix direccion en editarusuario

--- src/components/usuario/editarUser.vue
+++ src/components/usuario/editarUser.vue
@@ -27,91 +27,84 @@
                 <form class="needs-validation" novalidate>
                   <div class="form-row">
                     <div class="form-group col-md-6">
-                      <label>NOMBRE COMPLETO</label>
-                      <input
-                        v-validate="'required'"
-                        :class="{'is-invalid':errors.first('name')}"
-                        name="name"
-                        type="text"
-                        class="form-control"
-                        v-model="cliente.name"
-                      />
+                      <label>NOMBRE COMPLETO</label> <input
+                      v-validate="'required'"
+                      :class="{'is-invalid':errors.first('name')}"
+                      name="name"
+                      type="text"
+                      class="form-control"
+                      v-model="cliente.name"
+                    />
                       <div class="invalid-feedback">Se requiere un nombre.</div>
                     </div>
                     <div class="form-group col-md-6">
-                      <label>CEDULA</label>
-                      <input
-                        v-validate="'required'"
-                        @change="checkDocumento"
-                        :class="{'is-invalid':errors.first('cedula')}"
-                        name="cedula"
-                        type="text"
-                        class="form-control"
-                        v-model="cliente.identification"
-                      />
+                      <label>CEDULA</label> <input
+                      v-validate="'required'"
+                      @change="checkDocumento"
+                      :class="{'is-invalid':errors.first('cedula')}"
+                      name="cedula"
+                      type="text"
+                      class="form-control"
+                      v-model="cliente.identification"
+                    />
                       <div class="invalid-feedback">Se requiere de una cedula</div>
                     </div>
                     <div class="form-group col-md-12">
-                      <label>DIRECCI&Oacute;N</label>
-                      <input
-                        v-validate="'required'"
-                        :class="{'is-invalid':errors.first('direccion')}"
-                        name="direccion"
-                        type="text"
-                        class="form-control"
-                        v-model="address.address"
-                      />
+                      <label>DIRECCI&Oacute;N</label> <input
+                      v-validate="'required'"
+                      :class="{'is-invalid':errors.first('direccion')}"
+                      name="direccion"
+                      type="text"
+                      class="form-control"
+                      v-model="address.address"
+                    />
                       <div class="invalid-feedback">Se requiere de una direcci&oacute;n</div>
                     </div>
                     <div class="form-group col-md-6">
-                      <label>DEPARTAMENTO</label>
-                      <select
-                        @change="changeCiudades"
-                        v-validate="'required'"
-                        :class="{'is-invalid':errors.first('departamento')}"
-                        name="departamento"
-                        class="form-control"
-                        v-model="cliente.department"
-                      >
-                        <option v-for="item in departments" :value="item.id">{{item.name}}</option>
-                      </select>
+                      <label>DEPARTAMENTO</label> <select
+                      @change="changeCiudades"
+                      v-validate="'required'"
+                      :class="{'is-invalid':errors.first('departamento')}"
+                      name="departamento"
+                      class="form-control"
+                      v-model="cliente.department"
+                    >
+                      <option v-for="item in departments" :value="item.id">{{item.name}}</option>
+                    </select>
                       <div class="invalid-feedback">Se requiere de un departamento</div>
                     </div>
                     <div class="form-group col-md-6">
-                      <label>CIUDAD</label>
-                      <select
-                        v-validate="'required'"
-                        :class="{'is-invalid':errors.first('ciudad')}"
-                        name="ciudad"
-                        class="form-control"
-                        v-model="address.city_id"
-                      >
-                        <option v-for="item in cities" :value="item.id">{{item.name}}</option>
-                      </select>
+                      <label>CIUDAD</label> <select
+                      v-validate="'required'"
+                      :class="{'is-invalid':errors.first('ciudad')}"
+                      name="ciudad"
+                      class="form-control"
+                      v-model="address.city_id"
+                    >
+                      <option v-for="item in cities" :value="item.id">{{item.name}}</option>
+                    </select>
                       <div class="invalid-feedback">Se requiere de una ciudad</div>
                     </div>
                     <div class="form-group col-md-12">
-                      <label>CORREO</label>
-                      <input
-                        v-validate="'required|email'"
-                        :class="{'is-invalid':errors.first('email')}"
-                        name="email"
-                        type="email"
-                        class="form-control"
-                        v-model="cliente.email"
-                      />
+                      <label>CORREO</label> <input
+                      v-validate="'required|email'"
+                      :class="{'is-invalid':errors.first('email')}"
+                      name="email"
+                      type="email"
+                      class="form-control"
+                      v-model="cliente.email"
+                    />
                       <div class="invalid-feedback">Se requiere de un correo válido</div>
                     </div>
                     <div class="form-group col-md-6">
-                      <label>TELEFONO</label>
-                      <input
-                        v-validate="'required'"
-                        :class="{'is-invalid':errors.first('telefono')}"
-                        name="telefono"
-                        type="tel"
-                        class="form-control"
-                        v-model="cliente.phone"
-                      />
+                      <label>TELEFONO</label> <input
+                      v-validate="'required'"
+                      :class="{'is-invalid':errors.first('telefono')}"
+                      name="telefono"
+                      type="tel"
+                      class="form-control"
+                      v-model="cliente.phone"
+                    />
                       <div class="invalid-feedback">Se requiere de un tel&eacute;fono</div>
                     </div>
                     <div class="form-group col-md-12">
@@ -120,7 +113,8 @@
                           @click="next"
                           type="button"
                           class="btn btn-primary text-white btn-lg w-md-100 font-italic font-weight-bold"
-                        >Guardar</button>
+                        >Guardar
+                        </button>
                       </div>
                     </div>
                   </div>
@@ -133,12 +127,12 @@
     </div>
   </section>
 </template>
-
 <script>
-import api from "../../plugins/api";
+import api from '../../plugins/api'
+
 export default {
-  name: "editar-user",
-  data() {
+  name: 'editar-user',
+  data () {
     return {
       cities: [],
       cliente: {
@@ -147,105 +141,115 @@ export default {
         email: null,
         phone: null,
         department: null,
-        gender: "masculino"
+        gender: 'masculino'
       },
       address: {
         user_id: null,
-        name: "casa",
+        name: 'casa',
         address: null,
         city_id: null
       }
-    };
+    }
   },
-  mounted() {
+  mounted () {
     if (this.me.id) {
-      this.cliente = this.me;
+      this.cliente = this.me
       if (this.me.addresses.length > 0) {
-        let address = this.me.addresses[0];
-        this.address.city_id = address.city.id;
-        this.address.address = address.address;
-        this.address.user_id = address.user.id;
-        this.changeDepartment();
+        this.address = this.me.addresses[0]
+        this.changeDepartment()
       } else {
-        this.address.user_id = this.me.id;
+        this.address.user_id = this.me.id
       }
     }
   },
-  created() {
-    this.$store.dispatch("getDepartments");
-    this.$store.dispatch("getCities");
+  created () {
+    this.$store.dispatch('getDepartments')
+    this.$store.dispatch('getCities')
   },
   computed: {
-    departments() {
-      return this.$store.state.departments;
+    departments () {
+      return this.$store.state.departments
     },
-    citiesall() {
-      return this.$store.state.cities;
+    citiesall () {
+      return this.$store.state.cities
     },
-    me() {
-      return this.$store.state.me;
+    me () {
+      return this.$store.state.me
     }
   },
   methods: {
-    checkDocumento() {
+    checkDocumento () {
       api
         .Users()
         .checkDocumento(this.cliente.identification)
         .then(response => {
           if (response.data.data.length > 0) {
-            this.$store.commit("set_me", response.data.data[0]);
+            this.$store.commit('set_me', response.data.data[0])
             if (this.me.addresses.length > 0) {
-              this.address = this.me.addresses[0];
-              this.changeDepartment();
+              this.address = this.me.addresses[0]
+              this.changeDepartment()
             }
           } else if (!this.me.id) {
-            this.$store.commit("set_me", {});
-            this.cliente.id = null;
+            this.$store.commit('set_me', {})
+            this.cliente.id = null
           }
-        });
+        })
     },
-    next() {
+    next () {
       // cambiar direccion
-
+      if (!this.address.id) {
+        api.Addresses().create(this.address).then(response => {
+          this.address = response.data.data
+         
+        })
+      }else{
+        api.Addresses().update(this.address.id,this.address).then(response => {
+          this.address = response.data.data
+        })
+      }
       //
       
       api
         .Users()
         .update(this.me.id, this.cliente)
         .then(response => {
-          $('#modalEditar').modal('hide');
-        });
+          this.$store.commit('set_me', response.data.data)
+          //
+          $('#modalEditar').modal('hide')
+        })
     },
-    changeCiudades() {
+    changeCiudades () {
       this.cities = this.departments.filter(
         item => item.id == this.cliente.department
-      )[0].cities;
+      )[0].cities
     },
-    changeDepartment() {
+    changeDepartment () {
       this.cliente.department = this.departments.filter(
         item =>
           item.id ==
           this.citiesall.filter(node => node.id == this.address.city_id)[0]
             .department_id
-      )[0].id;
-      this.changeCiudades();
+      )[0].id
+      this.changeCiudades()
     }
   }
-};
+}
 </script>
-
 <style scoped>
-.caja {
-  border: 2px red solid;
-}
-
-label {
-  font-size: 0.7em;
-}
-
-.titulo {
-  margin-bottom: 2.5rem;
-}
-</style> 
-
+  .caja{
+    border: 2px red solid;
+  }
+  
+  label{
+    font-size: 0.7em;
+  }
+  
+  .titulo{
+    margin-bottom: 2.5rem;
+  }
 </style>
+</
+
+style
+
+>
