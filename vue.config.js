8cceb75e61cbe14550589dc83c72fc8c9003706f
mauricio suarez <mauroziux@gmail.com>
2019-10-30 13:51:18 -0500

    fix home google

--- vue.config.js
+++ vue.config.js
@@ -43,6 +43,19 @@ module.exports = {
 
     ]
   },
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+      
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+      
+    }
+  }
 
   
 }
