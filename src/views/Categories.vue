e7f46108c26b8e43cacf152d4f7543d61f5a5fa1
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-11-06 15:19:10 -0500

    detalles

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div id="productos">
     <volver></volver>
     <mercado :title="name"></mercado>
     <hoja></hoja>
-    <products :products="productos" id="productos"></products>
+    <products :products="productos"></products>
     <div class="col-12 justify-content-lg-end mb-5 p-2">
       <div class="d-flex justify-content-center" v-if="productos.length>0">
         <a href="#productos">
@@ -12,11 +12,11 @@
           @click="to_prev"
           icon="angle-left"
           size="lg"
-          class="separador mr-md-5"
+          class="separador mr-5"
         />
         </a>
         <a href="#productos">
-        <font-awesome-icon style="cursor:pointer" class="ml-md-5" @click="to_next" icon="angle-right" size="lg" />
+        <font-awesome-icon style="cursor:pointer" class="ml-5" @click="to_next" icon="angle-right" size="lg" />
         </a>
       </div>
     </div>
