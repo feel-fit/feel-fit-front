78bd83b38918e74f3fc3a32343b719b6f71c21ba
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-11-25 15:47:23 -0500

    recetas

--- src/components/recetas/home/instagram.vue
+++ src/components/recetas/home/instagram.vue
@@ -3,25 +3,27 @@
     <div class="row">
       <div class="col-2 text-uppercase">follow @instagram</div>
       <div class="col-10">
-        <hr/>
+        <hr />
       </div>
     </div>
     <div class="row">
       <div class="col">
-          <!-- LightWidget WIDGET -->
-          
-          <iframe src="http://instagram.com/p/a1wDZKopa2/juanlusuau" width="320" height="400" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+        <!-- LightWidget WIDGET -->
+        <iframe
+          src="//lightwidget.com/widgets/f7a16dcfa72856209afc560f9aaff9e4.html"
+          scrolling="no"
+          allowtransparency="true"
+          class="lightwidget-widget"
+          style="width:100%;border:0;overflow:hidden;"
+        ></iframe>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style scoped lang="scss">
-
 </style>
